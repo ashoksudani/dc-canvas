43dc0a93f87977c97a9a1b61bf4b49b017840f10
ashoksudani@gmail.com <ashoksudani@gmail.com>
2017-07-23 20:32:25 +0800

    [#0] : Ashok : Test refactoring

--- tests/integration/components/dc-canvas-test.js
+++ tests/integration/components/dc-canvas-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -20,15 +21,15 @@ test('it does not render canvas when entering invalid command', function(assert)
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{dc-canvas}}`);
-  $('#canvasInput').val('asdf asdfsda');
-  $('#canvasInput').change();
-  $('#canvasForm').submit();
+  Ember.$('#canvasInput').val('asdf asdfsda');
+  Ember.$('#canvasInput').change();
+  Ember.$('#canvasForm').submit();
 
   
   assert.equal(this.$('#canvasInput').length, 1);
-  assert.equal($('#dc-canvas').width(), 300);
-  assert.equal($('#dc-canvas').height(), 150);
-  assert.equal($('.ui.warning.message').get('length'), 1);
+  assert.equal(Ember.$('#dc-canvas').width(), 300);
+  assert.equal(Ember.$('#dc-canvas').height(), 150);
+  assert.equal(Ember.$('.ui.warning.message').get('length'), 1);
 });
 
 
@@ -36,53 +37,53 @@ test('it does not render canvas when entering invalid command', function(assert)
 test('it render canvas for canvas drawing command', function(assert) {
 
   this.render(hbs`{{dc-canvas}}`);
-  $('#canvasInput').val('C 120 200');
-  $('#canvasInput').change();
-  $('#canvasForm').submit();
+  Ember.$('#canvasInput').val('C 120 200');
+  Ember.$('#canvasInput').change();
+  Ember.$('#canvasForm').submit();
   
   assert.equal(this.$('#canvasInput').length, 1);
-  assert.equal($('#dc-canvas').width(), 120);
-  assert.equal($('#dc-canvas').height(), 200);
-  assert.equal($('.ui.warning.message').get('length'), 0);
+  assert.equal(Ember.$('#dc-canvas').width(), 120);
+  assert.equal(Ember.$('#dc-canvas').height(), 200);
+  assert.equal(Ember.$('.ui.warning.message').get('length'), 0);
 });
 
 test('it quits for quit command', function(assert) {
 
   this.render(hbs`{{dc-canvas}}`);
-  $('#canvasInput').val('Q');
-  $('#canvasInput').change();
-  $('#canvasForm').submit();
+  Ember.$('#canvasInput').val('Q');
+  Ember.$('#canvasInput').change();
+  Ember.$('#canvasForm').submit();
   
   assert.equal(this.$('#canvasInput').length, 1);
-  assert.equal($('#dc-canvas').width(), 300);
-  assert.equal($('#dc-canvas').height(), 150);
-  assert.equal($('.ui.warning.message').get('length'), 0);
+  assert.equal(Ember.$('#dc-canvas').width(), 300);
+  assert.equal(Ember.$('#dc-canvas').height(), 150);
+  assert.equal(Ember.$('.ui.warning.message').get('length'), 0);
 });
 
 test('it render canvas without any error for line drawing command', function(assert) {
 
   this.render(hbs`{{dc-canvas}}`);
-  $('#canvasInput').val('L 120 200 10 20');
-  $('#canvasInput').change();
-  $('#canvasForm').submit();
+  Ember.$('#canvasInput').val('L 120 200 10 20');
+  Ember.$('#canvasInput').change();
+  Ember.$('#canvasForm').submit();
 
   assert.equal(this.$('#canvasInput').length, 1);
-  assert.equal($('#dc-canvas').width(), 300);
-  assert.equal($('#dc-canvas').height(), 150);
-  assert.equal($('.ui.warning.message').get('length'), 0);
+  assert.equal(Ember.$('#dc-canvas').width(), 300);
+  assert.equal(Ember.$('#dc-canvas').height(), 150);
+  assert.equal(Ember.$('.ui.warning.message').get('length'), 0);
 });
 
 test('it render canvas without any error for bucket filling command', function(assert) {
 
   this.render(hbs`{{dc-canvas}}`);
-  $('#canvasInput').val('B 120 200 blue');
-  $('#canvasInput').change();
-  $('#canvasForm').submit();
+  Ember.$('#canvasInput').val('B 120 200 blue');
+  Ember.$('#canvasInput').change();
+  Ember.$('#canvasForm').submit();
 
   assert.equal(this.$('#canvasInput').length, 1);
-  assert.equal($('#dc-canvas').width(), 300);
-  assert.equal($('#dc-canvas').height(), 150);
-  assert.equal($('.ui.warning.message').get('length'), 0);
+  assert.equal(Ember.$('#dc-canvas').width(), 300);
+  assert.equal(Ember.$('#dc-canvas').height(), 150);
+  assert.equal(Ember.$('.ui.warning.message').get('length'), 0);
 });
 
 
